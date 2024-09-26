f6a15e7e87d140bf070e2dddf84b996498046f75
Donizeth <donizethf8@gmail.com>
2024-09-26 00:15:12 +0000

    BasicoProdutos

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Row(
+        children: [
+          NavigationRail(
+            selectedIndex: 0, //
+            onDestinationSelected: (int index) {},
+            labelType: NavigationRailLabelType.none,
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.person),
+                label: Text('Perfil'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.shopping_basket),
+                label: Text('Produtos'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.shopping_cart),
+                label: Text('Carrinho'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.help),
+                label: Text('Ajuda'),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Center(
+              child: Text('Conteúdo Principal'),
+            ),
+          ),
+        ],
       ),
     );
   }
